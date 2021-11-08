f14eddfe0d4ef222d3cb6291ea1f7662b00d32f2
Satyam Tomar <73239975+satyamtomar@users.noreply.github.com>
2021-11-08 19:30:40 +0530

    Update About.js

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -5,7 +5,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 const About = () => {
   const visitInstagram = () => {
-    window.location = "mailto:girisai.nemali1234@gmail.com";
+    window.location = "mailto:sargcom123@gmail.com";
   };
   return (
     <div className="aboutSection">
@@ -21,23 +21,23 @@ const About = () => {
               src="myimg.jpg"
               alt="Founder"
             />
-            <Typography>Giri SaiNath Reddy</Typography>
+            <Typography>SARGCOM</Typography>
             <Button onClick={visitInstagram} color="primary">
               Mail me
             </Button>
             <span>
-              This is an Ecommerce wesbite made by @Giri Sai. My purpose was to learn MERN stack in detail and implement the knowledge which learned during my Btech Career.
+              This is an Ecommerce wesbite made by @SATYAM TOMAR, @AYUSH MINA and @RAMESH VISHNOI . Our purpose was to squareboat hackathon.
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
-              href="https://github.com/codergiri12"
+              href="https://github.com/ayushmina/ECOMMERCE.2"
               target="blank"
             >
               <GitHubIcon className="youtubeSvgIcon" />
             </a>
-            <a href="https://www.linkedin.com/in/giri-sai-nath-reddy-4124731b0/" target="blank">
+            <a href="https://www.linkedin.com/school/national-institute-of-technology-kurukshetra/" target="blank">
               <LinkedInIcon className="instagramSvgIcon" />
             </a>
           </div>
