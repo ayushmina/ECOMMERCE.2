b4be656c9e4c08248b204d8dced70bdd37674aaa
ayushmina <66309981+ayushmina@users.noreply.github.com>
2021-11-08 17:24:14 +0530

    added
    frontend

---
+++ frontend/src/axiox.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+// const api = 'https://gyrokart-backend.herokuapp.com/'
+const api = 'http://localhost:4000'
+
+const axiosInstance = axios.create({
+});
+export default axiosInstance;
